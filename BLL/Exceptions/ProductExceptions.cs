538cb20d83c49c10a0b99b4568b6e887ea1537f1
sebastianpinzon19 <152411485+sebastianpinzon19@users.noreply.github.com>
2024-08-23 00:59:42 -0500

    feature/BLL
    BLL

---
+++ BLL/Exceptions/ProductExceptions.cs
@@ -0,0 +1,31 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Exceptions
+{
+    public class ProductExceptions : Exception
+    {
+        public ProductExceptions() : base("No product found in the database.")
+        {
+        }
+
+        private ProductExceptions(string message) : base(message)
+        {
+        }
+
+        public static void ThrowProductAlreadyExistsException(string productName, int? supplierId)
+        {
+            throw new ProductExceptions($"A product with the name '{productName} and supplier id {supplierId} already exists.");
+        }
+
+        public static void ThrowInvalidProductIdException(int id)
+        {
+            throw new ProductExceptions($"Invalid Product ID: {id}");
+        }
+    }
+}
+
